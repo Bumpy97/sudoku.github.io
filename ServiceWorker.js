e494dd8f0165a25d4c4bd5699fba0a301fba50dd
Bumpy97 <59314887+Bumpy97@users.noreply.github.com>
2024-05-17 16:58:13 +0400

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "chadgames-Sudoku-0.1";
-const contentToCache = [
-    "Build/ddd.loader.js",
-    "Build/ddd.framework.js.unityweb",
-    "Build/ddd.data.unityweb",
-    "Build/ddd.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
